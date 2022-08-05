1ef17303f9d0379e7e0715d58557eeed89526df7
Cris <cristinamihalciuc@yahoo.com>
2022-08-05 22:22:54 +0300

    Form initial display none

--- script.js
+++ script.js
@@ -33,7 +33,6 @@ const btn = document.getElementById('addBook');
 
 btn.addEventListener('click', () => {
   const form = document.getElementById('form');
-
   if (form.style.display === 'none') {
     form.style.display = 'block';
   } else {
